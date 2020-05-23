2a118f476ba99236ca14670047df16e645bc8287
Ricardo Vinicio Jara jara <5940607-RicardoVinicioJara@users.noreply.gitlab.com>
2020-05-22 22:33:59 -0500

    Vinos

--- app.py
+++ app.py
@@ -3,31 +3,29 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
-def calcular_similitud_jaccard(valores_vino):
+def similitudJaccard(valores_vino):
 
-    def calculo_similitud(calidad, set_valores_vino):
+    def similitud(calidad, set_valores_vino):
         return {
             'calidad': calidad,
-            'similitud': len(set_valores_vino.intersection(valores_vino)) /
-                         len(set_valores_vino.union(valores_vino))
+            'similitud': len(set_valores_vino.intersection(valores_vino)) / len(set_valores_vino.union(valores_vino))
         }
 
-    lista_calculada = []
+    list = []
     with open("winequality-red.csv", "r") as f:
         valores_vino_data = f.readline()
         while valores_vino_data:
             valores_vino_data = f.readline()
             propiedades_vino = valores_vino_data.split(";")
             calidad = propiedades_vino[-1:][0].replace("\n", "")
-            lista_calculada.append(calculo_similitud(calidad, set(map(float, propiedades_vino[:-1]))))
-    # Retorna las similitudes y su calidad
-    return sorted(lista_calculada, key=lambda item: item['similitud'], reverse=True)
+            list.append(similitud(calidad, set(map(float, propiedades_vino[:-1]))))
+    return sorted(list, key=lambda item: item['similitud'], reverse=True)
 
 
 @app.route('/calcular', methods=['POST'])
 def calcular_calidad():
     valores_vino = list(map(float, request.form.values()))
-    return render_template("index.html", valores=valores_vino, lista=calcular_similitud_jaccard(valores_vino))
+    return render_template("index.html", valores=valores_vino, lista=similitudJaccard(valores_vino))
 
 
 @app.route('/')
